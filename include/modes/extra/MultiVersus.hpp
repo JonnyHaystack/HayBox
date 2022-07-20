47ebe202c37ea8f93cab740c14a991a53fa36664
Robin Avery <robin@ribbanya.dev>
2022-07-20 17:04:21 -0400

    Create `MultiVersus` mode

---
+++ include/modes/extra/MultiVersus.hpp
@@ -0,0 +1,22 @@
+#ifndef _MODES_MULTIVERSUS_HPP
+#define _MODES_MULTIVERSUS_HPP
+
+#include "core/ControllerMode.hpp"
+#include "core/socd.hpp"
+#include "core/state.hpp"
+
+class MultiVersus : public ControllerMode {
+  public:
+    MultiVersus(socd::SocdType socd_type);
+
+  protected:
+    virtual void UpdateDigitalOutputs(InputState &inputs, OutputState &outputs);
+    virtual void UpdateAnalogOutputs(InputState &inputs, OutputState &outputs);
+
+  private:
+    bool horizontal_socd;
+
+    void HandleSocd(InputState &inputs);
+};
+
+#endif
---
+++ src/modes/extra/MultiVersus.cpp
@@ -0,0 +1,81 @@
+#include "modes/extra/MultiVersus.hpp"
+
+#define ANALOG_STICK_MIN 0
+#define ANALOG_STICK_NEUTRAL 128
+#define ANALOG_STICK_MAX 255
+
+MultiVersus::MultiVersus(socd::SocdType socd_type) : ControllerMode(socd_type) {
+    _socd_pair_count = 4;
+    _socd_pairs = new socd::SocdPair[_socd_pair_count]{
+        socd::SocdPair{&InputState::left,    &InputState::right  },
+        socd::SocdPair{ &InputState::down,   &InputState::up     },
+        socd::SocdPair{ &InputState::c_left, &InputState::c_right},
+        socd::SocdPair{ &InputState::c_down, &InputState::c_up   },
+    };
+
+    horizontal_socd = false;
+}
+
+void MultiVersus::HandleSocd(InputState &inputs) {
+    horizontal_socd = inputs.left && inputs.right;
+    InputMode::HandleSocd(inputs);
+}
+
+void MultiVersus::UpdateDigitalOutputs(InputState &inputs, OutputState &outputs) {
+    outputs.a = inputs.a;
+    outputs.b = inputs.b;
+    outputs.x = inputs.x;
+    outputs.y = inputs.y;
+    outputs.buttonR = inputs.z;
+    if (inputs.nunchuk_connected) {
+        outputs.triggerLDigital = inputs.nunchuk_z;
+    } else {
+        outputs.triggerLDigital = inputs.l;
+    }
+    outputs.triggerRDigital = inputs.r;
+    outputs.start = inputs.start;
+
+    // D-Pad layer can be activated by holding Mod X + Mod Y, or by holding the C
+    // button on a nunchuk.
+    if ((inputs.mod_x && inputs.mod_y) || inputs.nunchuk_c) {
+        outputs.dpadUp = inputs.c_up;
+        outputs.dpadDown = inputs.c_down;
+        outputs.dpadLeft = inputs.c_left;
+        outputs.dpadRight = inputs.c_right;
+    }
+
+    if (inputs.select)
+        outputs.dpadLeft = true;
+    if (inputs.home)
+        outputs.dpadRight = true;
+}
+
+void MultiVersus::UpdateAnalogOutputs(InputState &inputs, OutputState &outputs) {
+    // Coordinate calculations to make modifier handling simpler.
+    UpdateDirections(
+        inputs.left,
+        inputs.right,
+        inputs.down,
+        inputs.up,
+        inputs.c_left,
+        inputs.c_right,
+        inputs.c_down,
+        inputs.c_up,
+        ANALOG_STICK_MIN,
+        ANALOG_STICK_NEUTRAL,
+        ANALOG_STICK_MAX,
+        outputs
+    );
+
+    if ((inputs.mod_x && inputs.mod_y) || inputs.nunchuk_c) {
+        outputs.rightStickX = 0;
+        outputs.rightStickY = 0;
+    }
+
+    if (inputs.l) {
+        outputs.triggerLAnalog = 140;
+    }
+    if (inputs.r) {
+        outputs.triggerRAnalog = 140;
+    }
+}
