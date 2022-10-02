223ed3305bcbe8cb28e750043343f026687d7565
Nathan Koenigsmark <ndkoenigsmark@gmail.com>
2022-10-02 03:58:38 -0400

    updated with overflow angles

--- src/modes/UltPika.cpp
+++ src/modes/UltPika.cpp
@@ -49,6 +49,14 @@ void UltPika::UpdateAnalogOutputs(InputState &inputs, OutputState &outputs) {
         outputs.leftStickY = y_value;
     };
 
+    // set a tilt modifer if the rectangle is in "mode"
+    auto mode_set_stick = [&](int x_percent, int y_percent, bool down_only) {
+        if (inputs.lightshield) {
+            if (down_only && directions.y == 1) force_analog_stick(169, 128); 
+            else set_analog_stick(x_percent, y_percent);
+        }
+    };
+
     // Coordinate calculations to make modifier handling simpler.
     UpdateDirections(
         inputs.left,
@@ -84,28 +92,46 @@ void UltPika::UpdateAnalogOutputs(InputState &inputs, OutputState &outputs) {
     }
     if (inputs.l) outputs.triggerLAnalog = 140;
 
-    // quick attack mode
-    if (inputs.b) {
-        if (inputs.c_up) {force_analog_stick(169, 128); outputs.rightStickY = 128;} // double up zip
-        else if (inputs.c_left) {set_analog_stick(56, 83); outputs.rightStickX = 128;} // sliughtly steep
-        else if (inputs.a) {set_analog_stick(88, 47); outputs.a = false;} // shallow
-        else if (inputs.c_down) {set_analog_stick(93, 30); outputs.rightStickY = 128;} // very shallow
-        else if (inputs.c_right) {set_analog_stick(83, 56); outputs.rightStickX = 128;} // slightly shallow
-        else if (inputs.mod_x) set_analog_stick(56, 100); // very steep angle
-        else if (inputs.mod_y) set_analog_stick(35, 100); // steep angle 
-    }
-
-    // Qa2 OS c-stick angles
-    if (inputs.midshield) {
-        outputs.rightStickY = 28; outputs.rightStickX = 128;
-        // outputs.rightStickY = 57;
-        // outputs.rightStickX = 128 + directions.x * -57;
-    }
-
     // light sheild modifers
-    if (inputs.lightshield) {
+    if (inputs.lightshield) { 
         if (directions.vertical) force_analog_stick(169, 128); // double up zip
         else set_analog_stick(28, 100); // force jolt
+    }
+
+    // quick attack mode
+    if (inputs.b) {
+        if (inputs.c_up) { // double up zip
+            force_analog_stick(169, 128); 
+            outputs.rightStickY = 128; 
+        } else if (inputs.c_left) { // slightly steep
+            set_analog_stick(56, 83); 
+            mode_set_stick(88, 47, false);
+            outputs.rightStickX = 128;
+        } else if (inputs.a) { // shallow
+            set_analog_stick(88, 47); 
+            mode_set_stick(93, -30, false);
+            outputs.a = false;
+        } else if (inputs.c_down) { // very shallow
+            set_analog_stick(93, 30);
+            mode_set_stick(88, -47, false);
+            outputs.rightStickY = 128; 
+        } else if (inputs.c_right) { // slightly shallow
+            set_analog_stick(83, 56); 
+            mode_set_stick(35, 100, false);
+            outputs.rightStickX = 128;
+        } else if (inputs.mod_x) { // steep angle
+            set_analog_stick(56, 100);
+            mode_set_stick(-35, 100, true);
+        } else if (inputs.mod_y) { // very steep angle 
+            set_analog_stick(35, 100);
+            mode_set_stick(-56, 100, true);
+        }
+    }
+
+    // c-dash
+    if (inputs.midshield) {
+       outputs.rightStickY = 28; 
+       outputs.rightStickX = 128;
     }
 
     // Shut off A-stick when using D-Pad layer.
