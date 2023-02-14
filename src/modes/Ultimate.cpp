a992b99217679a7fe08f638dfa2e6e682fe08a04
Avahe Kellenberger <avahe@protonmail.ch>
2023-02-13 23:50:08 -0500

    ModX/Y changes
    - Can crouch on platforms with modX and modY
    - Can shield tilt with modY
    - ModX is now fastest walking speed

--- src/modes/Ultimate.cpp
+++ src/modes/Ultimate.cpp
@@ -8,7 +8,7 @@
 Ultimate::Ultimate(socd::SocdType socd_type) {
     _socd_pair_count = 4;
     _socd_pairs = new socd::SocdPair[_socd_pair_count]{
-        socd::SocdPair{&InputState::left,    &InputState::right,   socd_type},
+        socd::SocdPair{ &InputState::left,    &InputState::right,   socd_type},
         socd::SocdPair{ &InputState::down,   &InputState::up,      socd_type},
         socd::SocdPair{ &InputState::c_left, &InputState::c_right, socd_type},
         socd::SocdPair{ &InputState::c_down, &InputState::c_up,    socd_type},
