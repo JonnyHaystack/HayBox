b1794866188cc0c8f949d46e119b25c09092ab96
Eric Iniguez <colonket@iniguez.dev>
2023-06-03 19:45:21 -0500

    Patch conflicting outputs issue

--- src/modes/extra/TaikoDrums.cpp
+++ src/modes/extra/TaikoDrums.cpp
@@ -26,25 +26,25 @@ void TaikoDrums::UpdateDigitalOutputs(InputState &inputs, OutputState &outputs) 
     // LEFT RED
     // INPUTS: DOWN, RIGHT, MOD_Y
     // OUTPUTS: ( DPAD DOWN / DPAD RIGHT ) 
-    outputs.dpadDown = inputs.down;
+    outputs.dpadDown = inputs.down; 
     outputs.dpadRight = inputs.right;
-    outputs.dpadDown = inputs.mod_y;
+    //outputs.dpadDown = inputs.mod_y;
     // RIGHT BLUE
     // INPUTS: A, Z, UP
     // OUTPUTS: ( A / X / ZR / R )
-    outputs.a = inputs.a;
+    outputs.a = inputs.a; 
     outputs.buttonR = inputs.z;
     outputs.triggerRDigital = inputs.up;
     // RIGHT RED
     // INPUTS: C_DOWN, B, X
     // OUTPUTS: ( B / Y) 
-    outputs.b = inputs.c_down;
+    //outputs.b = inputs.c_down; 
     outputs.b = inputs.b;
     outputs.y = inputs.x;
     // Control Row (Right Hand Top Row)
     // INPUTS: R, Y, LS, MS
     // OUTPUTS: ( A / X / SELECT / HOME )
-    outputs.a = inputs.r;
+    //outputs.a = inputs.r;
     outputs.x = inputs.y;
     outputs.select = inputs.lightshield;
     outputs.home = inputs.midshield;
