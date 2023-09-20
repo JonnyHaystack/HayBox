c6928bf8699d824f35a2d88c9578435e56477379
jrorwin <43017573+jrorwin@users.noreply.github.com>
2023-09-19 20:38:37 -0700

    Update config.cpp

--- config/pico/config.cpp
+++ config/pico/config.cpp
@@ -27,33 +27,33 @@ KeyboardMode *current_kb_mode = nullptr;
 GamecubeControllerInput *gcc = nullptr;
 
 GpioButtonMapping button_mappings[] = {
-    // {&InputState::l,            5 },
-    // { &InputState::left,        4 },
-    // { &InputState::down,        3 },
-    // { &InputState::right,       2 },
+    {&InputState::l,            5 },
+    { &InputState::left,        4 },
+    { &InputState::down,        3 },
+    { &InputState::right,       2 },
 
-    // { &InputState::mod_x,       6 },
-    // { &InputState::mod_y,       7 },
+    { &InputState::mod_x,       6 },
+    { &InputState::mod_y,       7 },
 
-    // { &InputState::select,      10},
-    // { &InputState::start,       0 },
-    // { &InputState::home,        11},
+    { &InputState::select,      10},
+    { &InputState::start,       0 },
+    { &InputState::home,        11},
 
-    // { &InputState::c_left,      13},
-    // { &InputState::c_up,        12},
-    // { &InputState::c_down,      15},
-    // { &InputState::a,           14},
-    // { &InputState::c_right,     16},
+    { &InputState::c_left,      13},
+    { &InputState::c_up,        12},
+    { &InputState::c_down,      15},
+    { &InputState::a,           14},
+    { &InputState::c_right,     16},
  
-    // { &InputState::b,           26},
-    // { &InputState::x,           21},
-    // { &InputState::z,           19},
-    // { &InputState::up,          17},
+    { &InputState::b,           26},
+    { &InputState::x,           21},
+    { &InputState::z,           19},
+    { &InputState::up,          17},
 
-    // { &InputState::r,           27},
-    // { &InputState::y,           22},
-    // { &InputState::lightshield, 20},
-    // { &InputState::midshield,   18},
+    { &InputState::r,           27},
+    { &InputState::y,           22},
+    { &InputState::lightshield, 20},
+    { &InputState::midshield,   18},
 
     {&InputState::l,            5 },
     { &InputState::left,        4 },
@@ -82,7 +82,41 @@ GpioButtonMapping button_mappings[] = {
     { &InputState::y,           22},
     { &InputState::lightshield, 20},
     { &InputState::midshield,   18},
+
+
+
+    //LLODS layout
+
+    // { &InputState::l,           5 },
+    // { &InputState::left,        4 },
+    // { &InputState::down,        3 },
+    // { &InputState::right,       2 },
+
+    // { &InputState::mod_x,       6 },
+    // { &InputState::mod_y,       7 },
+
+    // { &InputState::select,      10},
+    // { &InputState::start,       0 },
+    // { &InputState::home,        11},
+
+    // { &InputState::c_left,      13},
+    // { &InputState::c_up,        12},
+    // { &InputState::c_down,      17},
+    // { &InputState::a,           14},
+    // { &InputState::c_right,     16},
+
+    // { &InputState::b,           15},
+    // { &InputState::x,           26},
+    // { &InputState::z,           19},
+    // { &InputState::up,          15},
+
+    // { &InputState::r,           21},
+    // { &InputState::y,           22},
+    // { &InputState::lightshield, 20},
+    // { &InputState::midshield,   18},
     
+
+
     //Swifts layout
 
     // {&InputState::l,            5 },
