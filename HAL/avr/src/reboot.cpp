50855e160b3f4cb64da8939cc7834f3380d05d2d
Jonathan Haylett <jonathan@haylett.dev>
2023-10-15 03:02:56 +0100

    feat: use watchdog and magic key for reboot firmware/bootloader on AVR

--- HAL/avr/src/reboot.cpp
+++ HAL/avr/src/reboot.cpp
@@ -2,13 +2,19 @@
 
 #include "stdlib.hpp"
 
-void (*resetFunc)(void) = 0;
+#include <avr/wdt.h>
 
 void reboot_firmware() {
-    resetFunc();
+    wdt_enable(WDTO_15MS);
+    while (true) {
+        delay(1);
+    }
 }
 
 void reboot_bootloader() {
+    uint16_t *magic_key_pos = (uint16_t *)MAGIC_KEY_POS;
+    *magic_key_pos = MAGIC_KEY;
+    wdt_enable(WDTO_15MS);
     while (true) {
         delay(1);
     }
