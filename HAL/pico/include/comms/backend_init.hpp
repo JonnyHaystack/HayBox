279b2028e78f7925b219db575226eaa7f5552607
Jonathan Haylett <jonathan@haylett.dev>
2023-11-11 15:36:50 +0000

    feat: making backend initialization stuff overridable

--- HAL/pico/include/comms/backend_init.hpp
+++ HAL/pico/include/comms/backend_init.hpp
@@ -17,7 +17,7 @@
  * @param input_source_count Number of elements in the input_sources array
  * @param config Reference to global config struct
  * @param pinout Pinout struct used for GameCube/N64 communication backends
- * @return size_t The number of backends in the array
+ * @return The number of backends in the array
  */
 size_t initialize_backends(
     CommunicationBackend **&backends,
@@ -27,5 +27,131 @@ size_t initialize_backends(
     Config &config,
     const Pinout &pinout
 );
+
+/**
+ * @brief Optionally defined function that allows a device config to hook into initialize_backends()
+ * and use a custom method for selecting backend config before console detection
+ *
+ * @param backend_config The reference to the current backend config after checking for button holds
+ * and connected console
+ * @param inputs Reference to the InputState struct to pass into each backend's constructor
+ * @param config Reference to the global Config struct
+ * @return true if the passed in backend config was altered, otherwise false
+ */
+bool backend_config_custom(
+    CommunicationBackendConfig &backend_config,
+    const InputState &inputs,
+    const Config &config
+) __attribute__((weak));
+
+/**
+ * @brief Optionally defined function that allows a device config to hook into initialize_backends()
+ * and use a custom method for selecting the default USB communication backend to initialize prior
+ * to console/USB detection
+ *
+ * @param backend_config The reference to the current backend config after checking for button holds
+ * and connected console
+ * @param config Reference to the global Config struct
+ * @return true if the passed in backend config was altered, otherwise false
+ */
+bool usb_backend_config_custom(CommunicationBackendConfig &backend_config, const Config &config)
+    __attribute__((weak));
+
+void detect_console_custom(Pinout &pinout);
+
+/**
+ * @brief Initialize primary backend based on the passed in backend id
+ *
+ * @param primary_backend The reference to the primary backend pointer to initialize
+ * @param backend_id The id of the backend to initialize
+ * @param backends The reference to assign to the created backends array
+ * @param inputs Reference to the InputState struct to pass into each backend's constructor
+ * @param input_sources Input sources array to pass into each backend's constructor
+ * @param input_source_count Number of elements in the input_sources array
+ * @param config Reference to global config struct
+ * @param pinout Pinout struct used for GameCube/N64 communication backends
+ */
+void init_primary_backend(
+    CommunicationBackend *&primary_backend,
+    CommunicationBackendId backend_id,
+    InputState &inputs,
+    InputSource **input_sources,
+    size_t input_source_count,
+    Config &config,
+    const Pinout &pinout
+);
+
+/**
+ * @brief Optionally defined function that allows a device config to hook into initialize_backends()
+ * and override initialization of primary backend
+ *
+ * @param primary_backend The reference to the primary backend pointer to initialize
+ * @param backend_id The id of the backend to initialize
+ * @param backends The reference to assign to the created backends array
+ * @param inputs Reference to the InputState struct to pass into each backend's constructor
+ * @param input_sources Input sources array to pass into each backend's constructor
+ * @param input_source_count Number of elements in the input_sources array
+ * @param config Reference to global config struct
+ * @param pinout Pinout struct used for GameCube/N64 communication backends
+ */
+void init_primary_backend_custom(
+    CommunicationBackend *&primary_backend,
+    CommunicationBackendId backend_id,
+    InputState &inputs,
+    InputSource **input_sources,
+    size_t input_source_count,
+    Config &config,
+    const Pinout &pinout
+) __attribute__((weak));
+
+/**
+ * @brief Initialize secondary backends and the backends array and return the number of elements in
+ * the array
+ *
+ * @param backends The reference to assign to the created backends array
+ * @param primary_backend The reference to the primary backend to insert first in the backends array
+ * @param backend_id The id indicating the primary backend
+ * @param inputs Reference to the InputState struct to pass into each backend's constructor
+ * @param input_sources Input sources array to pass into each backend's constructor
+ * @param input_source_count Number of elements in the input_sources array
+ * @param config Reference to global config struct
+ * @param pinout Pinout struct used for GameCube/N64 communication backends
+ * @return The number of backends in the array
+ */
+size_t init_secondary_backends(
+    CommunicationBackend **&backends,
+    CommunicationBackend *&primary_backend,
+    CommunicationBackendId backend_id,
+    InputState &inputs,
+    InputSource **input_sources,
+    size_t input_source_count,
+    Config &config,
+    const Pinout &pinout
+);
+
+/**
+ * @brief Optionally defined function that allows a device config to hook into initialize_backends()
+ * and override initialization of secondary backends
+ *
+ * @param backends The reference to assign to the created backends array
+ * @param primary_backend The reference to the primary backend to insert first in the backends array
+ * @param backend_id The id indicating the primary backend
+ * @param inputs Reference to the InputState struct to pass into each backend's constructor
+ * @param input_sources Input sources array to pass into each backend's constructor
+ * @param input_source_count Number of elements in the input_sources array
+ * @param config Reference to global config struct
+ * @param pinout Pinout struct used for GameCube/N64 communication backends
+ * @return The number of backends in the array
+ */
+size_t init_secondary_backends_custom(
+    CommunicationBackend **&backends,
+    CommunicationBackend *&primary_backend,
+    CommunicationBackendId backend_id,
+    InputState &inputs,
+    InputSource **input_sources,
+    size_t input_source_count,
+    Config &config,
+    const Pinout &pinout
+) __attribute__((weak));
 
 #endif
--- HAL/pico/src/comms/backend_init.cpp
+++ HAL/pico/src/comms/backend_init.cpp
@@ -27,62 +27,119 @@ size_t initialize_backends(
 ) {
     CommunicationBackend *primary_backend = nullptr;
 
+    /* First check button holds for a matching comms backend config. */
     CommunicationBackendConfig backend_config = backend_config_from_buttons(
         inputs,
         config.communication_backend_configs,
         config.communication_backend_configs_count
     );
+
+    // TODO: weakly defined backend_config_custom()
+
+    /* If no match found for button hold, use console/USB detection to select backend instead. */
     if (backend_config.backend_id == COMMS_BACKEND_UNSPECIFIED) {
-        primary_backend = new XInputBackend(inputs, input_sources, input_source_count);
-        CommunicationBackendId detected_backend_id = detect_console(pinout);
-        backend_config = backend_config_from_id(
-            detected_backend_id,
-            config.communication_backend_configs,
-            config.communication_backend_configs_count
+        /* Must check default USB backend here and initialize it before console detection, so that
+         * we can respond correctly to device descriptor requests from host. */
+        const CommunicationBackendConfig &usb_backend_config =
+            config.communication_backend_configs[config.default_usb_backend_config - 1];
+        // TODO: weakly defined usb_backend_config_custom()
+        init_primary_backend(
+            primary_backend,
+            usb_backend_config.backend_id,
+            inputs,
+            input_sources,
+            input_source_count,
+            config,
+            pinout
         );
+        // TODO: weakly defined detect_console_custom()
+        CommunicationBackendId detected_backend_id = detect_console(pinout);
+        if (detected_backend_id == COMMS_BACKEND_XINPUT) {
+            backend_config = usb_backend_config;
+        } else {
+            backend_config = backend_config_from_id(
+                detected_backend_id,
+                config.communication_backend_configs,
+                config.communication_backend_configs_count
+            );
+        }
+    }
+    if (backend_config.backend_id == COMMS_BACKEND_UNSPECIFIED &&
+        config.default_backend_config > 0) {
+        backend_config = config.communication_backend_configs[config.default_backend_config - 1];
     }
 
     size_t backend_count;
 
-    switch (backend_config.backend_id) {
+    // TODO: weakly defined init_primary_backend_custom()
+    init_primary_backend(
+        primary_backend,
+        backend_config.backend_id,
+        inputs,
+        input_sources,
+        input_source_count,
+        config,
+        pinout
+    );
+    // TODO: weakly defined init_secondary_backends_custom()
+    backend_count = init_secondary_backends(
+        backends,
+        primary_backend,
+        backend_config.backend_id,
+        inputs,
+        input_sources,
+        input_source_count,
+        config,
+        pinout
+    );
+
+    if (backend_config.default_mode_config > 0) {
+        const GameModeConfig &mode_config =
+            config.game_mode_configs[backend_config.default_mode_config - 1];
+        set_mode(primary_backend, mode_config, config.keyboard_modes, config.keyboard_modes_count);
+    }
+
+    return backend_count;
+}
+
+void init_primary_backend(
+    CommunicationBackend *&primary_backend,
+    CommunicationBackendId backend_id,
+    InputState &inputs,
+    InputSource **input_sources,
+    size_t input_source_count,
+    Config &config,
+    const Pinout &pinout
+) {
+    switch (backend_id) {
         case COMMS_BACKEND_DINPUT:
-            delete primary_backend;
-            TUGamepad::registerDescriptor();
-            TUKeyboard::registerDescriptor();
-            primary_backend = new DInputBackend(inputs, input_sources, input_source_count);
-            backend_count = 2;
-            backends = new CommunicationBackend *[backend_count] {
-                primary_backend, new B0XXInputViewer(inputs, input_sources, input_source_count)
-            };
+            if (primary_backend == nullptr) {
+                TUGamepad::registerDescriptor();
+                TUKeyboard::registerDescriptor();
+                primary_backend = new DInputBackend(inputs, input_sources, input_source_count);
+            }
+            break;
+        case COMMS_BACKEND_NINTENDO_SWITCH:
+            if (primary_backend == nullptr) {
+                NintendoSwitchBackend::RegisterDescriptor();
+                primary_backend =
+                    new NintendoSwitchBackend(inputs, input_sources, input_source_count);
+            }
             break;
         case COMMS_BACKEND_XINPUT:
-            // delete primary_backend;
-            // primary_backend = new XInputBackend(inputs, input_sources, input_source_count);
-            backend_count = 2;
-            backends = new CommunicationBackend *[backend_count] {
-                primary_backend, new B0XXInputViewer(inputs, input_sources, input_source_count)
-            };
+            if (primary_backend == nullptr) {
+                primary_backend = new XInputBackend(inputs, input_sources, input_source_count);
+            }
             break;
         case COMMS_BACKEND_GAMECUBE:
             delete primary_backend;
             primary_backend =
                 new GamecubeBackend(inputs, input_sources, input_source_count, pinout.joybus_data);
-            backend_count = 1;
-            backends = new CommunicationBackend *[backend_count] { primary_backend };
             break;
         case COMMS_BACKEND_N64:
             delete primary_backend;
             primary_backend =
                 new N64Backend(inputs, input_sources, input_source_count, pinout.joybus_data);
-            backend_count = 1;
-            backends = new CommunicationBackend *[backend_count] { primary_backend };
-            break;
-        case COMMS_BACKEND_NINTENDO_SWITCH:
-            delete primary_backend;
-            NintendoSwitchBackend::RegisterDescriptor();
-            primary_backend = new NintendoSwitchBackend(inputs, input_sources, input_source_count);
-            backend_count = 1;
-            backends = new CommunicationBackend *[backend_count] { primary_backend };
             break;
         case COMMS_BACKEND_UNSPECIFIED: // Fall back to configurator if invalid backend selected.
         case COMMS_BACKEND_CONFIGURATOR:
@@ -90,23 +147,32 @@ size_t initialize_backends(
             delete primary_backend;
             primary_backend =
                 new ConfiguratorBackend(inputs, input_sources, input_source_count, config);
+    }
+}
+
+size_t init_secondary_backends(
+    CommunicationBackend **&backends,
+    CommunicationBackend *&primary_backend,
+    CommunicationBackendId backend_id,
+    InputState &inputs,
+    InputSource **input_sources,
+    size_t input_source_count,
+    Config &config,
+    const Pinout &pinout
+) {
+    size_t backend_count = 0;
+
+    switch (backend_id) {
+        case COMMS_BACKEND_DINPUT:
+        case COMMS_BACKEND_XINPUT:
+            backend_count = 2;
+            backends = new CommunicationBackend *[backend_count] {
+                primary_backend, new B0XXInputViewer(inputs, input_sources, input_source_count)
+            };
+        default:
             backend_count = 1;
             backends = new CommunicationBackend *[backend_count] { primary_backend };
     }
-
-    GameModeId mode_id = backend_config.default_mode;
-    if (backend_config.default_mode == MODE_UNSPECIFIED) {
-        mode_id = MODE_MELEE;
-    }
-
-    set_mode(
-        primary_backend,
-        mode_id,
-        config.game_mode_configs,
-        config.game_mode_configs_count,
-        config.keyboard_modes,
-        config.keyboard_modes_count
-    );
 
     return backend_count;
 }
