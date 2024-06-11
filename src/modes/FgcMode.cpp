d80420c9d1ca74cff6d018e197bf26c71d34cf42
Jonathan Haylett <jonathan@haylett.dev>
2024-06-11 20:58:10 +0100

    feat: add default mappings for L3 and R3 in FgcMode

--- src/modes/FgcMode.cpp
+++ src/modes/FgcMode.cpp
@@ -13,6 +13,8 @@ void FgcMode::UpdateDigitalOutputs(const InputState &inputs, OutputState &output
     outputs.start = inputs.mb1;
     outputs.select = inputs.rt3;
     outputs.home = inputs.rt2;
+    outputs.leftStickClick = inputs.rt1;
+    outputs.rightStickClick = inputs.rt5;
 
     // Right hand bottom row
     outputs.a = inputs.rf1;
