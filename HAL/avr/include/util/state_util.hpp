60f589e0cb1ceb84f44e5a8f6daf8a3e7d915c55
Jonathan Haylett <jonathan@haylett.dev>
2025-03-14 23:30:27 +0000

    Add missing AVR state_util.hpp

--- HAL/avr/include/util/state_util.hpp
+++ HAL/avr/include/util/state_util.hpp
@@ -399,4 +399,81 @@ inline bool all_buttons_held(const uint64_t &buttons, uint64_t button_mask) {
     return button_mask != 0 && (buttons & button_mask) == button_mask;
 }
 
+inline bool any_button_held(const uint64_t &buttons, uint64_t button_mask) {
+    return button_mask != 0 && (buttons & button_mask);
+}
+
+/* OutputState utils */
+
+inline void set_output(uint32_t &buttons, DigitalOutput output_index, bool pressed) {
+    if (output_index == GP_UNSPECIFIED) {
+        return;
+    }
+    DigitalOutput output_index_adjusted = (DigitalOutput)(output_index - 1);
+    buttons =
+        (buttons & ~(1UL << output_index_adjusted)) | ((uint32_t)pressed << output_index_adjusted);
+}
+
+inline uint8_t OutputState::*axis_pointer(AnalogAxis axis) {
+    switch (axis) {
+        case AXIS_LSTICK_X:
+            return &OutputState::leftStickX;
+        case AXIS_LSTICK_Y:
+            return &OutputState::leftStickY;
+        case AXIS_RSTICK_X:
+            return &OutputState::rightStickX;
+        case AXIS_RSTICK_Y:
+            return &OutputState::rightStickY;
+        case AXIS_LTRIGGER:
+            return &OutputState::triggerLAnalog;
+        case AXIS_RTRIGGER:
+            return &OutputState::triggerRAnalog;
+        default:
+            return nullptr;
+    }
+}
+
+constexpr const char *digital_output_name(DigitalOutput output) {
+    switch (output) {
+        case GP_A:
+            return "A";
+        case GP_B:
+            return "B";
+        case GP_X:
+            return "X";
+        case GP_Y:
+            return "Y";
+        case GP_LB:
+            return "L1";
+        case GP_RB:
+            return "R1";
+        case GP_LT:
+            return "L2";
+        case GP_RT:
+            return "R2";
+        case GP_START:
+            return "Start";
+        case GP_SELECT:
+            return "Select";
+        case GP_HOME:
+            return "Home";
+        case GP_CAPTURE:
+            return "Capture";
+        case GP_DPAD_UP:
+            return "D-Pad Up";
+        case GP_DPAD_DOWN:
+            return "D-Pad Down";
+        case GP_DPAD_LEFT:
+            return "D-Pad Left";
+        case GP_DPAD_RIGHT:
+            return "D-Pad Right";
+        case GP_LSTICK_CLICK:
+            return "L3";
+        case GP_RSTICK_CLICK:
+            return "R3";
+        default:
+            return "Unknown";
+    }
+}
+
 #endif
